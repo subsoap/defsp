2de144fe23720efbdb014324fcc29d03985aaf0c
Brian Kramer <subsoap@gmail.com>
2017-08-13 17:25:38 -0700

    More setup

---
+++ utils/audio.go
@@ -0,0 +1,33 @@
+components {
+  id: "audio"
+  component: "/utils/audio.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "audio_global"
+  type: "collectionproxy"
+  data: "collection: \"/game/assets/audio/global.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
